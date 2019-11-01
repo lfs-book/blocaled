9bdf7739aa111ff5a0d0e3da4880ce3a4db50a29
pierre <pierre.labastie@neuf.fr>
2019-11-01 17:02:37 +0100

    Mock dbus system for tests
    Use a bash coproc: allows to retrieve output from a background
                       job
    export DBUS_SYSTEM_BUS_ADDRESS so that mylocaled believes it
    is on a system message bus
    keep DBUS_PID for killing it at the end

--- tests/test-env.sh
+++ tests/test-env.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-dbus-daemon --config-file=scratch/test-session.xml &
+coproc dbus-daemon --config-file=scratch/test-session.xml \
+	           --print-address=1 \
+		   --print-pid=1
+read -u $COPROC DBUS_SYSTEM_BUS_ADDRESS
+export DBUS_SYSTEM_BUS_ADDRESS
+read -u $COPROC DBUS_PID
