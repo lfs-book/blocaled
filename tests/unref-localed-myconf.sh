957df87762aa8e02b6f59ef33a530d86ab419937
Pierre Labastie <pierre.labastie@neuf.fr>
2019-11-15 15:41:56 +0100

    Add an utility to start the daemon with a custom config in tests

---
+++ tests/unref-localed-myconf.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#  Copyright 2019 Pierre Labastie
+#
+#  Permission is hereby granted, free of charge, to any person obtaining
+#  a copy of this software and associated documentation files (the
+#  "Software"), to deal in the Software without restriction, including
+#  without limitation the rights to use, copy, modify, merge, publish,
+#  distribute, sublicense, and/or sell copies of the Software, and to
+#  permit persons to whom the Software is furnished to do so, subject to
+#  the following conditions:
+#
+#  The above copyright notice and this permission notice shall be
+#  included in all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+#  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+#  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+#  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+(
+flock -x 9
+read -u 9 nb_links
+if ((nb_links == 1)); then
+    kill $(cat scratch/mylocaled.pid)
+    rm scratch/mylocaled.pid
+    trap "rm myloc-lock" 0
+else
+    echo $((nb_links - 1)) > myloc-lock
+    flock -u  9
+fi
+) 9<>myloc-lock
